1554f5b6d9bafac6cc29f54dbfe92172f1430dd8
ISABELpolania18 <isabel.polania@udea.edu.co>
2025-04-28 10:58:20 -0500

    transpuesta y F, C

--- taller.py
+++ taller.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-vector = np.random.rand(720).reshape(120,6)
+vector = np.random.rand(720).reshape(120,6).T
+F = np.array(vector, order='F')
+C = np.array(vector, order='C')
