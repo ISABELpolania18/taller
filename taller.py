9890fa952d5903fc4c104efe7abd9c212a62b696
ISABELpolania18 <isabel.polania@udea.edu.co>
2025-04-28 13:31:47 -0500

    Pie

--- taller.py
+++ taller.py
@@ -9,9 +9,11 @@ fig = plt.figure(figsize=(12, 6))
 ax1 = fig.add_axes([0.05, 0.55, 0.25, 0.25])
 ax2 = fig.add_axes([0.35, 0.55, 0.25, 0.25])
 ax3 = fig.add_axes([0.65, 0.55, 0.25, 0.25])
+ax4 = fig.add_axes([0.05, 0.1, 0.25, 0.25])
+ax5 = fig.add_axes([0.35, 0.1, 0.25, 0.25])
 
 #Plot
-ax1.plot(vector[0])
+ax1.plot(vector[0], color ='r')
 
 #Scatter
 x = np.arange(120)
@@ -21,7 +23,13 @@ ax2.scatter(x,vector[1])
 dy = 0.1
 ax3.errorbar(x, vector[2], yerr=dy, fmt = '.k', ecolor='pink')
 
+#Histograma
+ax4.hist(vector[3], bins=30, alpha=0.5, color='orange', edgecolor='black')
 
+#Pie
+labels = 'Valor 1', 'Valor 2', 'Valor 3', 'Valor 4'
+colors = 'orange', 'red', 'pink', 'green'
+ax5.pie(vector[4][0:4], labels = labels, colors = colors, autopct='%1.1f%%')
 
 plt.show()
 
