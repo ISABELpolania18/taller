d77177676884bfd50aa152415d6507407264f823
ISABELpolania18 <isabel.polania@udea.edu.co>
2025-04-28 13:05:45 -0500

    scatter y correciones

--- taller.py
+++ taller.py
@@ -5,12 +5,16 @@ vector = np.random.rand(720).reshape(120,6).T
 F = np.array(vector, order='F')
 C = np.array(vector, order='C')
 
-fig = plt.figure()
-ax1 = fig.add_axes([0.1, 0.1, 0.8, 0.4])
-ax2 = fig.add_axes([0.1, 0.5, 0.8, 0.4])
+fig = plt.figure(figsize=(12, 8))
+ax1 = fig.add_axes([0.1, 0.55, 0.35, 0.35])
+ax2 = fig.add_axes([0.55, 0.55, 0.35, 0.35])
 
 #Plot
-ax1.plot(vector[1])
+ax1.plot(vector[0])
+
+#Scatter
+x = np.arange(120)
+ax2.scatter(x,vector[1])
 
 plt.show()
 
